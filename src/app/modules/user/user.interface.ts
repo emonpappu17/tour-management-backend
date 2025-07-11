0565527317b19056a1b51b6b73710437a5e3a80b
emonpappu17 <emon.pappu.5@gmail.com>
2025-07-11 20:48:56 +0600

    add zod validation for user

--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -12,7 +12,7 @@ export enum Role {
 // google authentication
 
 export interface IAuthProvider {
-    provider: string; // "Google", "Credential"
+    provider: "google" | "credentials"; // "Google", "Credential"
     providerId: string;
 
 }
--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { UserController } from "./user.controller";
+import { createUserZodSchema } from './user.validation'
+import { validateRequest } from "../../../middlewares/validateRequest";
 
 const router = Router()
 
-router.post("/register", UserController.createUser)
+router.post("/register", validateRequest(createUserZodSchema), UserController.createUser)
 router.get("/all-users", UserController.getAllUsers)
 
 export const UserRoutes = router
