416114fa78339a581f0a7d01249fcc9c2ecea066
emonpappu17 <emon.pappu.5@gmail.com>
2025-07-25 11:44:58 +0600

    add sendEmail with nodemailer

--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -13,6 +13,10 @@ router.post("/logout", AuthControllers.logout)
 router.post("/change-password", checkAuth(...Object.values(Role)), AuthControllers.changePassword)
 router.post("/reset-password", checkAuth(...Object.values(Role)), AuthControllers.resetPassword)
 router.post("/set-password", checkAuth(...Object.values(Role)), AuthControllers.setPassword)
+router.post("/forgot-password", AuthControllers.setPassword)
+
+// Frontend -> forget-password -> email -> user status check -> short expiration token (valid for 10 min) -> email -> Fronted Link http://localhost:5173/reset-password?email=saminisrar1@gmail.com&token=token -> frontend e  query theke user er email and token extract anbo -> new password user theke nibe -> backend er /reset-password api -> authorization = token -> newPassword -> token verify -> password hash -> save user password   
+
 
 // /booking -> /login -> successful google login -> /booking frontend
 // /login -> successful google login -> / frontend
