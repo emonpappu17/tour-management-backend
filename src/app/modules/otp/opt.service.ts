56a31608fe6acc9376c5ad8eae58eb54d02a5d07
emonpappu17 <emon.pappu.5@gmail.com>
2025-07-26 14:05:34 +0600

    add verify otp api and generatePdf

--- src/app/modules/otp/opt.service.ts
+++ src/app/modules/otp/opt.service.ts
@@ -1,6 +1,8 @@
 import crypto from "crypto";
 import { redisClient } from "../../config/redis.config";
 import { sendEmail } from "../../utils/sendEmail";
+import AppError from "../../errorHelpers/AppError";
+import { User } from "../user/user.model";
 
 const OTP_EXPIRATION = 2 * 60 //2 minute
 
@@ -14,6 +16,16 @@ const generateOtp = (length = 6) => {
 }
 
 const sendOTP = async (email: string, name: string) => {
+    const user = await User.findOne({ email })
+
+    if (!user) {
+        throw new AppError(404, "User not found")
+    }
+
+    if (user.isVerified) {
+        throw new AppError(404, "You are already verified")
+    }
+
     const otp = generateOtp();
 
     const redisKey = `otp:${email}`
@@ -36,8 +48,35 @@ const sendOTP = async (email: string, name: string) => {
     })
 };
 
-const verifyOTP = async () => {
-    return
+const verifyOTP = async (email: string, otp: string) => {
+
+    // const user = await User.findOne({ email, isVerified: false })
+    const user = await User.findOne({ email })
+
+    if (!user) {
+        throw new AppError(404, "User not found")
+    }
+
+    if (user.isVerified) {
+        throw new AppError(404, "You are already verified")
+    }
+
+    const redisKey = `otp:${email}`
+
+    const saveOpt = await redisClient.get(redisKey);
+
+    if (!saveOpt) {
+        throw new AppError(401, "Invalid OTP");
+    }
+
+    if (saveOpt !== otp) {
+        throw new AppError(401, "Invalid OTP");
+    }
+
+    await Promise.all([
+        User.updateOne({ email }, { isVerified: true }, { runValidators: true }),
+        redisClient.del([redisKey])
+    ])
 };
 
 export const OTPService = {
